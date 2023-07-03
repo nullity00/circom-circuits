eb92121d8dc41c3b6060e323a87b0a0ffd2aa11e
nullity00 <reshmashaik3011@gmail.com>
2023-07-03 20:30:14 +0530

    tests

--- test/basic.js
+++ test/basic.js
@@ -2,8 +2,8 @@ const path = require("path");
 const assert = require("chai").assert;
 const wasm_tester = require("circom_tester").wasm;
 
-describe("CheckAddition", () => {
-  var circ_file = path.join(__dirname, "circuits", "basic.circom");
+describe("Check Addition", () => {
+  var circ_file = path.join(__dirname, "circuits", "add.circom");
   var circ, num_constraints;
 
   before(async () => {
@@ -13,13 +13,33 @@ describe("CheckAddition", () => {
       console.log("Number of constraints: " + num_constraints);
   });
 
-  it("Addition of 3 and 4", async () => {
+  it("Addition of 3, 4, 5, 6", async () => {
       const input = {
-          "a": "3",
-          "b": "4"
+          "a": ["3", "4", "5", "6"],
       };
       const witness = await circ.calculateWitness(input);
       await circ.checkConstraints(witness);
-      await circ.assertOut(witness, {"c": "7"});
+      await circ.assertOut(witness, {"out": "18"});
   });
 });
+
+describe("Check Multiplication", () => {
+    var circ_file = path.join(__dirname, "circuits", "mul.circom");
+    var circ, num_constraints;
+  
+    before(async () => {
+        circ = await wasm_tester(circ_file);
+        await circ.loadConstraints();
+        num_constraints = circ.constraints.length;
+        console.log("Number of constraints: " + num_constraints);
+    });
+  
+    it("Multiplication of 3, 4, 5, 6", async () => {
+        const input = {
+            "a": ["3", "4", "5", "6"],
+        };
+        const witness = await circ.calculateWitness(input);
+        await circ.checkConstraints(witness);
+        await circ.assertOut(witness, {"out": "360"});
+    });
+  });
